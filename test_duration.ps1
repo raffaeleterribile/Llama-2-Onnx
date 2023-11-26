ba48e465a458e0f865316885683f90a625965394
Raffaele Terribile <raff-github@usa.net>
2023-11-26 10:32:27 +0100

    Add powershell script to test performance

---
+++ test_duration.ps1
@@ -0,0 +1,3 @@
+Get-Date
+python MinimumExample/Example_ONNX_LlamaV2.py --onnx_file 7B_FT_float16/ONNX/LlamaV2_7B_FT_float16.onnx --embedding_file 7B_FT_float16/embeddings.pth --tokenizer_path tokenizer.model --prompt "What is the lightest element?"
+Get-Date
